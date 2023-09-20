0b9b3c55f962914233aa6bfd50a0c25e8fc6ecc8
Hetvi Shah <hetvishah@usc-guestwireless-upc-newsc9699.usc.edu>
2023-09-19 22:13:28 -0700

    Added Pointer Movement and text question letter collection.

---
+++ 2D Space/Assets/Scripts/PointerMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerMovement : MonoBehaviour
+
+{
+    private Transform m_transform;
+    public float horizontalInput;
+    private float speed = 2.0f;
+    private float Horizontalspeed = 10.0f;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        m_transform = this.transform;
+
+    }
+
+    private void LAMouse()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - m_transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        //this is the angle that the weapon must rotate around to face the cursor
+        Quaternion rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward); //Vector 3. forward is Z a› (a localvariable float angle
+        m_transform.rotation = rotation;
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+        transform.Translate(Vector2.up * Horizontalspeed * Time.deltaTime * horizontalInput);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        LAMouse();
+
+
+    }
+}
--- 2D Space/Assets/Scripts/usedLetterCollision.cs
+++ 2D Space/Assets/Scripts/usedLetterCollision.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Text;
+using TMPro;
 
 public class usedLetterCollision : MonoBehaviour
 {
     public GameObject spaceShip;
+    public string answer = "JUPITER";
+    public int index;
+    private StringBuilder collectedLetters = new StringBuilder();
+    public TextMeshProUGUI collectedLettersText;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -13,13 +19,24 @@ public class usedLetterCollision : MonoBehaviour
         BoxCollider2D spaceCollider = spaceShip.GetComponent<BoxCollider2D>();
         if (collision.collider == spaceCollider)
         {
-            Destroy(gameObject);
+            string letter = this.gameObject.name; // Get the letter's name or identifier
+            Debug.Log(letter);
+            Debug.Log(answer.Contains(letter));
+            if (answer.Contains(letter))
+            {
+                index = answer.IndexOf(letter);
+                collectedLettersText.text = collectedLettersText.text.Remove(index * 2, 1).Insert(index * 2, letter);
+               
+            }
             Debug.Log(collision.collider);
+            Destroy(gameObject);
+
             // Pick the letter and fill the blank
         }
         else
         {
             Destroy(collision.gameObject);
+            Destroy(gameObject);
         }
         
     }
